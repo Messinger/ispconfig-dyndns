d2d7220ba14873670a36e32c02b366f7f59be5f7
Rajko Albrecht <ral@alwins-world.de>
2014-05-03 21:10:39 +0200

    hash keys always as symbol

--- app/models/isp_client.rb
+++ app/models/isp_client.rb
@@ -10,26 +10,32 @@ class IspClient
   def self.client_get_by_username asession,username
     return if asession.blank? || !asession.valid?
     r = super(:message => {:sessionid => asession.sessionid, :username => username})
+    #self.response_to_hash r
     clientvalues = self.flatten_hash(self.response_to_hash(r))
     IspClient.new clientvalues
   end
 
-  def self.client_get asession,id
+  def self.client_get asession,aclient
     return if asession.blank? || !asession.valid?
+    if aclient.is_a? IspClient
+      id = aclient.clientdata[:client_id]
+    else
+      id = aclient
+    end
     r = super(:message => {:sessionid => asession.sessionid, :client_id => id})
     clientvalues = self.flatten_hash(self.response_to_hash(r))
     IspClient.new clientvalues
   end
 
-  def client
-    @clientdata[:item]
+  def clientdata
+    @clientdata
   end
 
   private
  
   def self.flatten_hash r
     _h = r[:item]
-    Hash[*_h.map {|v| [v[:key],self.value_to_string(v[:value])]}.flatten]
+    Hash[*_h.map {|v| [v[:key].to_sym,self.value_to_string(v[:value])]}.flatten]
   end
 
 end
