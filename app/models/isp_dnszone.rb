3a387cdedfbea67cd1287abf4d67b5d36e043063
Rajko Albrecht <ral@alwins-world.de>
2014-05-04 14:04:36 +0200

    raise RecordNotFound errors

--- app/models/isp_dnszone.rb
+++ app/models/isp_dnszone.rb
@@ -33,16 +33,19 @@ class IspDnszone
   def self.dns_zone_get_by_user asession,aclient,serverid
     return if asession.blank? || !asession.valid?
     r = super(:message => {:sessionid => asession.sessionid, :client_id => aclient.clientdata[:client_id],:server_id => serverid})
-    zones = (self.response_to_hash(r)[:item]).collect do |zone| 
-        vals = flatten_hash zone 
-        i = IspDnszone.new(vals[:id],vals[:origin])
-      end
+    r = self.response_to_hash r
+    raise ActiveRecord::RecordNotFound unless r.has_key?(:item)
+    zones = (r[:item]).collect do |zone| 
+      vals = flatten_hash zone 
+      i = IspDnszone.new(vals[:id],vals[:origin])
+    end
   end
 
   def self.dns_zone_get asession,id
     return if asession.blank? || !asession.valid?
-    r = super(:message => {:sessionid => asession.sessionid, :primary_id => id})
-    data = flatten_hash self.response_to_hash(r)
+    r = self.response_to_hash super(:message => {:sessionid => asession.sessionid, :primary_id => id})
+    raise ActiveRecord::RecordNotFound if r == false
+    data = flatten_hash r
     IspDnszone.new(data[:id],data[:origin],data)
   end
 
