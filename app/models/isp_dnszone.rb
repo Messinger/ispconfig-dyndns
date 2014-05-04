969444f063d2791e12f794278bd1c348fc0e742b
Rajko Albrecht <ral@alwins-world.de>
2014-05-04 15:22:09 +0200

    auto handling incoming SOAP attributes into model attributes

--- app/models/isp_dnszone.rb
+++ app/models/isp_dnszone.rb
@@ -1,35 +1,11 @@
-class IspDnszone
-  include ActiveModel::Serializers::JSON
+class IspDnszone < PresentationModel
   extend Ispremote
 
-  operations :dns_zone_get_by_user, :dns_zone_get
+  attr_accessor :sys_userid, :sys_groupid, :origin, :sys_perm_user, :sys_perm_group, :sys_perm_other, :server_id, :ns, :mbox, :serial, :refresh
+  attr_accessor :retry, :expire, :minimum, :ttl, :active, :xfer, :also_notify, :update_acl
   
-  def initialize aid,aorigin,data=nil
-    @id = aid
-    @origin = aorigin
-    @data = data
-  end
-
-  def id
-    @id
-  end  
-
-  def id=(id)
-    @id = id
-  end
-
-  def origin
-    @origin
-  end
-
-  def origin=(origin)
-    @origin = origin
-  end
-
-  def data
-    @data
-  end
-
+  operations :dns_zone_get_by_user, :dns_zone_get, :dns_rr_get_all_by_zone
+  
   def self.dns_zone_get_by_user asession,aclient,serverid
     return if asession.blank? || !asession.valid?
     r = super(:message => {:sessionid => asession.sessionid, :client_id => aclient.clientdata[:client_id],:server_id => serverid})
@@ -37,7 +13,7 @@ class IspDnszone
     raise ActiveRecord::RecordNotFound unless r.has_key?(:item)
     zones = (r[:item]).collect do |zone| 
       vals = flatten_hash zone 
-      i = IspDnszone.new(vals[:id],vals[:origin])
+      i = IspDnszone.new(vals)
     end
   end
 
@@ -46,7 +22,16 @@ class IspDnszone
     r = self.response_to_hash super(:message => {:sessionid => asession.sessionid, :primary_id => id})
     raise ActiveRecord::RecordNotFound if r == false
     data = flatten_hash r
-    IspDnszone.new(data[:id],data[:origin],data)
+    IspDnszone.new(data)#[:id],data[:origin],data)
+  end
+  
+  def dns_rr_get_all_by_zone asession
+    return if asession.blank? || !asession.valid?
+    r = IspDnszone.response_to_hash super(:message => {:sessionid => asession.sessionid, :primary_id => self.id})
+    raise ActiveRecord::RecordNotFound unless r.has_key?(:item)
+    rrs = (r[:item]).collect do |rr|
+      IspDnszone.flatten_hash rr
+    end
   end
 
 end
---
+++ app/models/presentation_model.rb
@@ -0,0 +1,84 @@
+
+# Application Wide Model
+# Provides main functionality for models that are used in views 
+class PresentationModel
+  include ActiveModel::Serializers::JSON
+  include ActiveModel::Serializers::Xml
+  include ActiveModel::Validations
+  include ActiveModel::Validations::Callbacks
+  include ActiveModel::Conversion
+  extend  ActiveModel::Naming
+  
+  attr_accessor :id
+  
+  def set_id(id)
+    @id = id
+  end
+  
+  # copied from ActiveRecord
+  def persisted?
+    !(self.id.nil?)
+  end
+  
+  # copied from ActiveRecord
+  def new_record?
+    !persisted?
+  end
+  
+  # Single attribute validation method
+  def self.valid_attribute?(attr, value)
+    mock = self.new(attr => value)
+    unless mock.valid?
+      if mock.errors.has_key?(attr)
+        return mock.errors[attr] 
+      end
+    end
+    true
+  end
+  
+  def self.valid_attribute_set?(attribute_set, instance)
+    instance.errors.clear
+    
+    if instance.valid?
+      return Array.new()
+    end
+    
+    result_arr = Hash.new()
+    attribute_set.each do | attribute |
+      if instance.errors.has_key? attribute
+        if instance.errors[attribute].is_a?(Array)
+          errors = Array.new()
+          instance.errors[attribute].each do | attribute_error |
+            errors << attribute_error
+          end
+          result_arr[attribute] = errors
+        else
+          result_arr[attribute] = instance.errors[attribute]
+        end
+      end
+    end
+    instance.errors.clear
+    return result_arr
+  end
+  
+  # Generic initialization method to init Instances with every instance attribute
+  def initialize(*h)
+    if h.length == 1 && h.first.kind_of?(Hash)
+      h.first.each { |k,v| send("#{k}=",v) }
+    end
+  end
+  
+  
+  # List of attributes, used by as_json
+  alias :attributes :instance_values
+  
+  # List of attributes to exclude on serialization
+  def self.active_model_attributes
+    [
+      :errors,
+      :validation_context,
+    ]
+  end
+  
+  
+end
