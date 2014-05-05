b86987fc9f65b62621acf189456be43e9d9704ee
Rajko Albrecht <ral@alwins-world.de>
2014-05-05 10:12:22 +0200

    start sessions table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140503082128) do
+ActiveRecord::Schema.define(version: 20140505072621) do
+
+  create_table "sessions", force: true do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "s_id_idx"
+  add_index "sessions", ["updated_at"], name: "s_update_idx"
 
   create_table "settings", force: true do |t|
     t.string   "name",       default: "", null: false
