aa32825114b594265ecda90ea24340cfdacda5a0
Rajko Albrecht <ral@alwins-world.de>
2014-05-12 21:03:45 +0200

    we need the client id for a client user

--- app/models/client_user.rb
+++ app/models/client_user.rb
@@ -2,11 +2,14 @@
 class ClientUser 
   include User
   
+  attr_accessor :client_id
+  
   def initialize(isp_client)
     self.login_id = isp_client.username
     self.authentication_token = isp_client.passwort
     self.password = isp_client.passwort
     self.id = isp_client.id
+    self.client_id = isp_client.client_id
   end
 
 end
