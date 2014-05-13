63cd0fa09b1352c8d1f27d6f476a37f3471144ef
Rajko Albrecht <ral@alwins-world.de>
2014-05-13 22:43:15 +0200

    model attribute reordering

--- app/models/local_user.rb
+++ app/models/local_user.rb
@@ -1,8 +1,6 @@
 class LocalUser < ActiveRecord::Base
   include User
-  
-  attr_accessor :first_name, :last_name, :email
-  
+    
   validates :first_name, :presence => true
   validates :last_name, :presence => true
   validates :email, :presence => true, :uniqueness => true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 module User
 
   attr_accessor :login_id, :password
- 
+  attr_accessor :first_name, :last_name, :email
+  
   def admin?
     false
   end
