c5694fcebe8d9d9d0aad6b335b9268f9392b953c
Rajko Albrecht <ral@alwins-world.de>
2014-05-15 23:38:16 +0200

    getter/setter for ispdnszone for dnszone model

--- app/models/dns_zone.rb
+++ app/models/dns_zone.rb
@@ -6,4 +6,20 @@ class DnsZone < ActiveRecord::Base
   validates :isp_client_user_id, :presence => true
   validates :name, :presence => true, :uniqueness => true
 
+  def ispdnszone=(ispdnszone)
+      self.isp_dnszone_id = ispdnszone.id
+      self.isp_dnszone_origin = ispdnszone.origin.dup
+      self.isp_client_user_id = ispdnszone.sys_userid
+      name = self.isp_dnszone_origin.dup
+      while name.last == '.' do 
+          name.chomp!('.')
+      end
+      self.name = name
+      self
+  end
+  
+  def ispdnszone
+      @ispdnszone ||= IspDnszone.dns_zone_get self.isp_dnszone_id
+  end
+
 end
