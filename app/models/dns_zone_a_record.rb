02cf0f0328871332646ee11eaf32d0c9ddf20530
Rajko Albrecht <ral@alwins-world.de>
2014-05-17 00:25:48 +0200

    first validators for a/aaaa record model

--- app/models/dns_zone_a_record.rb
+++ app/models/dns_zone_a_record.rb
@@ -1,4 +1,6 @@
 class DnsZoneARecord < ActiveRecord::Base
   belongs_to :dns_zone_record
   
+  validates :dns_zone_record_id, :presence => true
+  
 end
--- app/models/dns_zone_aaaa_record.rb
+++ app/models/dns_zone_aaaa_record.rb
@@ -1,5 +1,6 @@
 class DnsZoneAaaaRecord < ActiveRecord::Base
   belongs_to :dns_zone_record
   
+  validates :dns_zone_record_id, :presence => true
   
 end
