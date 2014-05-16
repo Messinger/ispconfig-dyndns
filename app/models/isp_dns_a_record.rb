dbc9754f6ebb4a45b362551b733d45dbb4cf470c
Rajko Albrecht <ral@alwins-world.de>
2014-05-17 00:25:23 +0200

    try to send new A-Records but fails on ispconfig side due bugs in it...

--- app/models/isp_dns_a_record.rb
+++ app/models/isp_dns_a_record.rb
@@ -1,6 +1,6 @@
 class IspDnsARecord < IspResourceRecord
 
-  operations :dns_a_get
+  operations :dns_a_get, :dns_a_add, :dns_a_update, :dns_a_delete
 
   def initialize ahash=nil
     super ahash
@@ -16,6 +16,34 @@ class IspDnsARecord < IspResourceRecord
     r 
   ensure
     asession.logout
+  end
+
+  def self.dns_a_add zonerecord,client
+    asession = IspSession.login
+    clientid = client.client_id
+    serverid = client.default_dnsserver
+    rec = IspDnsARecord.new
+    
+    rec = {
+        :server_id => serverid,
+        :zone => zonerecord.dns_zone.isp_dnszone_id,
+        :type => "a",
+        :data => zonerecord.dns_zone_a_record.address,
+        :name => zonerecord.name,
+        :ttl => "300",
+        :active => "y",
+        :aux => "0",
+        :serial => "1",
+        :stamp => "#{Time.now.to_i}"
+    }
+    
+    message = { :session_id => asession.sessionid, :client_id => clientid, :params => rec}
+
+    p message
+    result = super(:message => message)
+    result.body
+  ensure
+    asession.logout unless asession.nil?
   end
 
 end
