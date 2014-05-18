326a2073db9599b4936b7c2bc3b5d6d79bc0e46d
Rajko Albrecht <ral@alwins-world.de>
2014-05-18 20:59:29 +0200

    dns AAAA records functions works

--- app/models/dns_zone_aaaa_record.rb
+++ app/models/dns_zone_aaaa_record.rb
@@ -2,5 +2,11 @@ class DnsZoneAaaaRecord < ActiveRecord::Base
   belongs_to :dns_zone_record
   
   validates :dns_zone_record_id, :presence => true
-  
+
+  def to_ispconfig_hash
+    {
+      :data => self.address
+    }.merge self.dns_zone_record.to_ispconfig_hash
+  end
+
 end
--- app/models/isp_dns_aaaa_record.rb
+++ app/models/isp_dns_aaaa_record.rb
@@ -1,6 +1,6 @@
 class IspDnsAaaaRecord < IspResourceRecord
   
-  operations :dns_aaaa_get
+  operations :dns_aaaa_get, :dns_aaaa_add, :dns_aaaa_update, :dns_aaaa_delete
   
   def initialize ahash=nil
     super ahash
@@ -17,4 +17,60 @@ class IspDnsAaaaRecord < IspResourceRecord
   ensure
     asession.logout
   end
+
+  def self.dns_aaaa_add arecord,client
+    asession = IspSession.login
+    clientid = client.client_id.to_s
+    serverid = client.default_dnsserver.to_i
+    recordhash = arecord.to_ispconfig_hash.merge(default_ispconfig_hash)
+    recordhash = recordhash.merge({:server_id => serverid})
+
+    rec = { :item =>
+            recordhash.collect { |k,v| {:key => k, :value => v, :attributes! => self.send("attributes_for_#{v.class.name.underscore}") } }
+          }
+    message = { :param0 => asession.sessionid, :param1 => clientid, :param2 => rec, :attributes! => { :param0 => {"xsi:type" => "xsd:string"}, :param1 => { "xsi:type" => "xsd:int" }, :param2 => {"xsi:type" => "ns2:Map" } } }
+
+    Rails.logger.debug message
+
+    result = super(:message => message)
+    result.body[:dns_aaaa_add_response][:return]
+
+  ensure
+    asession.logout unless asession.nil?
+  end
+
+  def dns_aaaa_update arecord,client
+    asession = IspSession.login
+    clientid = client.client_id.to_s
+    primaryid = self.id.to_i
+    recordhash = arecord.to_ispconfig_hash.merge(IspDnsAaaaRecord.default_ispconfig_hash)
+    rec = { :item =>
+            recordhash.collect { |k,v| {:key => k, :value => v, :attributes! => IspDnsAaaaRecord.send("attributes_for_#{v.class.name.underscore}") } }
+          }
+    message = { :param0 => asession.sessionid, :param1 => clientid, :param2 => primaryid, :param3 => rec, :attributes! => { :param0 => {"xsi:type" => "xsd:string"}, :param1 => { "xsi:type" => "xsd:int" }, :param2 => {"xsi:type" => "xsd:string"}, :param3 => {"xsi:type" => "ns2:Map" } } }
+    result = super(:message => message)
+    result.body[:dns_aaaa_update_response][:return]
+  ensure
+    asession.logout unless asession.nil?
+  end
+
+  def dns_aaaa_delete
+    asession = IspSession.login
+    result = super(:message => { :session_id => asession.sessionid, :primary_id => self.id} )
+    result.body[:dns_aaaa_delete_response][:return]
+  ensure
+    asession.logout unless asession.nil?
+  end
+
+  def self.default_ispconfig_hash
+    {
+      :type => "AAAA",
+      :aux => "0",
+      :active => "y",
+      :ttl => "300",
+      :serial => "CURRENT_TIMESTAMP",
+      :stamp =>  "CURRENT_TIMESTAMP"
+    }
+  end
+
 end
