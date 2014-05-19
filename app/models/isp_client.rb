2cdb74025968c1491b102e551df6b3e0788cb924
Rajko Albrecht <ral@alwins-world.de>
2014-05-19 10:55:35 +0200

    added method getting IspClient for a given isp client user

--- app/models/isp_client.rb
+++ app/models/isp_client.rb
@@ -2,15 +2,20 @@ class IspClient < PresentationModel
   extend Ispremote::Soap
   include UserHelper::GeneralUser
   
-  operations :client_get
+  operations :client_get, :client_get_id
 
   def initialize data
     super data
     set_id client_id
   end
 
-  def self.client_get aclientuser
-    asession = IspSession.login
+  # get Client by client ID
+  def self.client_get aclientuser, usesession=nil
+    if usesession.nil?
+      asession = IspSession.login
+    else
+      asession = usesession
+    end
     if aclientuser.is_a? IspClientUser
       id = aclientuser.client_id
     else
@@ -21,7 +26,36 @@ class IspClient < PresentationModel
     clientvalues = self.flatten_hash(r)
     IspClient.new clientvalues
   ensure 
-    asession.logout unless asession.nil?
+    asession.logout if !asession.nil? && usesession.nil?
   end
 
+  # get client id assigned to client _user_ id!
+  def self.client_get_id aclientuser,usesession=nil
+    if usesession.nil?
+      asession = IspSession.login
+    else
+      asession = usesession
+    end
+    if aclientuser.is_a? IspClientUser
+      id = aclientuser.userid
+    else
+      id = aclientuser
+    end
+    begin
+      self.response_to_hash super(:message => {:sessionid => asession.sessionid, :client_id => id})
+    rescue Savon::SOAPFault => ex
+      raise ActiveRecord::RecordNotFound
+    end
+  ensure 
+    asession.logout if !asession.nil? && usesession.nil?
+  end
+  
+  def self.client_for_user aclientuser
+    asession = IspSession.login
+    client_id = self.client_get_id aclientuser,asession
+    self.client_get client_id,asession
+  ensure
+    asession.logout unless asession.nil?
+  end
+  
 end
