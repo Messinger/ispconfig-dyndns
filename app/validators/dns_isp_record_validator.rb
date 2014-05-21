c1b782c045ae93f7c75558c1974fa811e8c16bd7
Rajko Albrecht <ral@alwins-world.de>
2014-05-21 20:08:29 +0200

    refactoring typo corrected

--- app/validators/dns_isp_record_validator.rb
+++ app/validators/dns_isp_record_validator.rb
@@ -4,7 +4,7 @@ class DnsIspRecordValidator <  ActiveModel::Validator
     if record.new_record? || record.name_changed? || record.dns_zone_id_changed?
       record.errors[:dnszone] << "Dnszone must selected" and return if record.dns_zone.nil?
       record.errors[:name] << "No record name set" and return if record.name.blank?
-      recs = record.dns_zone.ispdnszone.find_record_by_name record.name
+      recs = record.dns_zone.isp_dnszone.find_record_by_name record.name
       if recs.length > 0
         irecaid= record.dns_zone_a_record.isp_dns_a_record_id.to_s
         irecaaaaid = record.dns_zone_aaaa_record.isp_dns_aaaa_record_id.to_s
