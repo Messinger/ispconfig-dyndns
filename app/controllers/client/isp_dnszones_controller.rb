0938e980b2574f44c8a99137ba267865c4044032
Rajko Albrecht <ral@alwins-world.de>
2014-05-21 11:36:18 +0200

    Breadcrumbs for isp_dnszones_controller

--- app/controllers/client/isp_dnszones_controller.rb
+++ app/controllers/client/isp_dnszones_controller.rb
@@ -5,11 +5,16 @@ class Client::IspDnszonesController < ApplicationController
     cu = ClientUserDecorator.new(current_user)
     @ispclient = cu.client
     @ispdnszones = IspDnszone.dns_zone_get_by_user cu,@ispclient.default_dnsserver
+    cu = ClientUserDecorator.new current_user
+    add_breadcrumb "ISPConfig domains for #{cu.full_name}",client_isp_dnszones_path
   end
 
   def show
     @ispdnszone = IspDnszone.dns_zone_get params[:id]
     raise ForbiddenRequest.new if current_user.id != @ispdnszone.sys_userid
+    cu = ClientUserDecorator.new current_user
+    add_breadcrumb "ISPConfig domains for #{cu.full_name}",client_isp_dnszones_path
+    add_breadcrumb @ispdnszone.origin,client_isp_dnszone_path(@ispdnszone)
   end
 
 end
