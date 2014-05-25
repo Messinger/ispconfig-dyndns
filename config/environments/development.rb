eb39fb0d44f87e52f3c78b3b9faef22dee090008
Rajko Albrecht <ral@alwins-world.de>
2014-05-25 23:00:38 +0200

    disabled assets logging

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,8 @@ RailsDynamicDomain::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
+  config.assets.logger = nil
   config.assets.initialize_on_precompile = true
   
   config.log_level = :debug
