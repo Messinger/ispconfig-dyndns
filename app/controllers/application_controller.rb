b9cf7f31943436a5103b85a56eefe6f98a36812c
Rajko Albrecht <ral@alwins-world.de>
2014-05-27 11:16:34 +0200

    Removed obsolete controller methods/comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,15 +6,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+ 
   before_action :authenticate_user!
   
-#  before_filter :set_authentication_information
-  before_filter :process_authentication
-
   check_authorization :unless => :devise_controller? 
-
-#  helper_method :current_user
     
   def initialize
     @current_client_user = nil
@@ -25,25 +20,6 @@ class ApplicationController < ActionController::Base
     @current_client_user ||= session[:current_client_user]
   end
   
-  # The current logged in user
-  #
-  # == Returns
-  # a User object if a user is logged in and authorized. If no user is logged in, nil is returned.
- # def current_user
- #   @current_user ||= session[:current_user]
- #   if @current_user.nil?
- #     # if not logged in check if a apitoken is used
- #     apitoken = params[:apitoken]
- #     unless apitoken.blank?
- #       key = ApiKey.find_by_access_token apitoken
- #       unless key.blank?
- #         return key
- #       end
- #     end
- #   end
- #   return @current_user
- # end
-
   # Create and return a request assigened Ability object.
   #
   # It gets the current running request as second parameter so CanCan may check against values in request.
@@ -56,45 +32,6 @@ class ApplicationController < ActionController::Base
 
   private
   
-#  def set_authentication_information
-#    add_breadcrumb "Home", :root_path
-#
-#    if current_user
-#      if current_user.instance_of? ClientUser
-#        add_breadcrumb "Client user home", :client_root_path
-#      end
-#    end
-#    # touch session object so updated_at is set
-#    session[:lastseen] = Time.now()
-#    # remove outdated sessions
-#    Session.sweep(24.hours)
-#  end
-  
-  def process_authentication
-    Rails.logger.debug(session.inspect)
-#    if(is_authenticated_session)
-#      return true
-#    else
-#      respond_with_no_valid_authentication_found
-#      return false
-#    end
-  end
-  
-  def is_authenticated_session
-    return !current_user.nil?
-  end
-
-#  def respond_with_no_valid_authentication_found
-#    respond_to do |format|
-#      format.html {
-#        redirect_to user_login_path and return
-#        # TODO flash out a message
-#      }
-#      format.json {
-#        render(nothing: true, status: :unauthorized)
-#      }
-#    end
-#  end
   
   rescue_from RequestException do |exception|
     if exception.is_a? SecurityException
