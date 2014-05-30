184b9e455f7a9a7a062714158f9557c252ad600a
Rajko Albrecht <ral@alwins-world.de>
2014-05-31 00:43:23 +0200

    try a update admin data method (not yet working)

--- app/controllers/admins/admins_controller.rb
+++ app/controllers/admins/admins_controller.rb
@@ -5,9 +5,33 @@ class Admins::AdminsController < ApplicationController
     @admin = current_admin
     raise NotFound.new if @admin.nil?
     authorize! :edit, @admin
+    resource = @admin
   end
 
   def update
+    @admin = current_admin
+    raise NotFound.new if @admin.nil?
+    authorize! :edit, @admin
+    raise BadRequest.new unless params.has_key? :admin
+
+    @admin.update_with_password admin_params
+
+    if !@admin.valid?
+      render 'edit' and return
+    end
+
+    redirect_to admin_edit_path
+
+  end
+
+  def resource
+    @admin
+  end
+
+  private
+
+  def admin_params
+    params.require(:admin).permit(:email, :password, :current_password, :password_confirmation)
   end
 
 end
