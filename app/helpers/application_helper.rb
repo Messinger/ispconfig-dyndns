6a18e8fc32032155add81ff0533c469e9a471c79
Rajko Albrecht <ral@alwins-world.de>
2014-11-23 22:25:08 +0100

    Moved over jquery ui theme to public and use a helper including them into app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,14 @@ module ApplicationHelper
     return "#{page_title}".html_safe unless page_title.empty?
     "IspConfig Dyndns broker"
   end
+
+  # Much like usual rails route helpers,
+  # whether you realized it or not, you can pass
+  # option :only_path => false to get a
+  # full URL instead of a
+  # relative path.
+  def public_static_path(path, options = nil)
+    root_path(options) + path
+  end
+
 end
