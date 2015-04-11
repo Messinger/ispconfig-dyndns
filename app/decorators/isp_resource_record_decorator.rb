c313b6a63a2ce697ff06dd37724f10acfde76414
Rajko Albrecht <ral@alwins-world.de>
2015-04-11 19:28:31 +0200

    show link to local record if exist for remote record

--- app/decorators/isp_resource_record_decorator.rb
+++ app/decorators/isp_resource_record_decorator.rb
@@ -14,10 +14,16 @@ class IspResourceRecordDecorator < ApplicationDecorator
     @table_hash ||= retrieve_table_hash
   end
 
+  def local_record_url
+    _r = localdns
+    return "" if _r.blank?
+    h.link_to _r.name,h.dns_host_record_path(_r)
+  end
+
   private
 
   def retrieve_table_hash
-    {:name => model.name, :data => model.data, :type => model.type, :localdns => localdnsname }
+    {:name => model.name, :data => model.data, :type => model.type, :localrecord => local_record_url }
   end
 
 end
