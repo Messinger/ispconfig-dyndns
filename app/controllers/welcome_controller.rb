020632bea74edec8fecd4c1810b0a0b25647bc29
Rajko Albrecht <ral@alwins-world.de>
2016-03-28 14:58:18 +0200

    json output for index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,25 @@
 class WelcomeController < ApplicationController
 
+  respond_to :json, :html
+
   skip_before_filter :process_authentication
   skip_authorization_check :only => [ :index, :admin_index, :user_index, :client_index ]
 
   def index
+    if request.format.json?
+      render nothing: true
+    end
   end
 
   def client_index
   end
 
   def admin_index
+
+    if request.format.json?
+      render nothing: true
+    end
+
   end
 
   def user_index
