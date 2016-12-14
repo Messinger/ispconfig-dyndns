dd7bef5d3b221a7fdd40cce9d8f891ca18035e7a
Rajko Albrecht <ral@alwins-world.de>
2016-12-14 14:39:41 +0100

    some layout stuff

---
+++ config/initializers/assets.rb
@@ -0,0 +1,3 @@
+Rails.application.config.assets.precompile += %w( pages/*/*  datatable/* dataTables/* )
+Rails.application.config.assets.precompile += %w( jquery.dataTables.css )
+Rails.application.config.assets.precompile += %w( dataTables.bootstrap.css )
