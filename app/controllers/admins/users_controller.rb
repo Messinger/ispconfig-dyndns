e18b5b6df28d1dae36d8ea971f63e21a7374c2ba
Rajko Albrecht <ral@alwins-world.de>
2019-02-12 15:26:09 +0100

    Prepare destroy user by admin

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -23,4 +23,28 @@ class Admins::UsersController < ApplicationController
     end
   end
 
+  def destroy
+    user = User.accessible_by(current_ability).find params[:id]
+    status = if user.nil?
+               :not_found
+             else
+              records = DnsHostRecordDecorator.decorate_collection(user.dns_host_records)
+              records.each do |record|
+                record.delete_remote
+              end
+              user.destroy
+              :ok
+             end
+
+    respond_to do |format|
+      format.html {
+        redirect_to admins_users_path
+      }
+      format.json {
+         render json: {}, status: status
+      }
+    end
+
+  end
+
 end
