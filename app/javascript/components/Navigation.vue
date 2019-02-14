977e9dd0f6a4ee6805039e5c098589853d6094db
Rajko Albrecht <ral@alwins-world.de>
2019-02-14 15:19:17 +0100

    try start login dialog

--- app/javascript/components/Navigation.vue
+++ app/javascript/components/Navigation.vue
@@ -3,22 +3,38 @@
         <v-toolbar-side-icon></v-toolbar-side-icon>
         <v-toolbar-title>Dyndns</v-toolbar-title>
         <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat to="/">
-            Home
-        </v-btn>
-        <v-btn flat to="/dns-host-records" v-if="loggedIn()">
-            Records
-        </v-btn>
+            <v-btn flat to="/">
+                Home
+            </v-btn>
+            <v-btn flat to="/dns-host-records" v-if="isUser()">
+                Records
+            </v-btn>
+        </v-toolbar-items>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down" v-if="!loggedIn()">
+            <v-btn flat>
+                Domain admin
+            </v-btn>
+            <v-btn flat>
+                Platform admin
+            </v-btn>
         </v-toolbar-items>
     </v-toolbar>
 </template>
 
 <script>
+    import loginDialog from 'components/shared/login_dialog'
     export default {
         name: "Navigation",
         methods: {
             loggedIn() {
                 return !(window === undefined || window.Constants === undefined || window.Constants.current_user === null);
+            },
+            isClient() {
+                return this.loggedIn() && window.Constants.current_user.type === 'ClientUser'
+            },
+            isUser() {
+                return this.loggedIn() && window.Constants.current_user.type === 'User'
             }
         }
     }
---
+++ app/javascript/components/shared/login_dialog.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "login_dialog",
+        data: () => ({
+            dialog: false,
+            login_url: '/users/sign_in'
+        })
+    }
+</script>
+
+<style scoped>
+
+</style>
