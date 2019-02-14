f2fa683accc0cdbe47ebc32c039948a78a436a81
Rajko Albrecht <ral@alwins-world.de>
2019-02-14 09:07:33 +0100

    use vuetify package
    Including devel fixes from
    https://github.com/mysticaltech/rails-vuetify-test
    
    (bug: https://github.com/vuetifyjs/vuetify/issues/4068)

--- app/javascript/packs/hello_vue.coffee
+++ app/javascript/packs/hello_vue.coffee
@@ -4,10 +4,12 @@
   import VueResource from 'vue-resource'
   import VueRouter from 'vue-router';
   import App from '../app.vue'
+  import Vuetify from "vuetify"
   import { routes } from './routes'
 
   Vue.use(VueResource)
   Vue.use(VueRouter)
+  Vue.use(Vuetify)
 
   router = new VueRouter({
     routes
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,6 +2,11 @@ const { environment } = require('@rails/webpacker')
 const coffee =  require('./loaders/coffee')
 const vue =  require('./loaders/vue')
 
+const customConfig = require('./custom')
+
+// Merge custom config
+environment.config.merge(customConfig)
+
 environment.loaders.append('vue', vue)
 environment.loaders.append('coffee', coffee)
 module.exports = environment
