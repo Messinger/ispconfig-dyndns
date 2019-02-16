8f34881468d567de830e8ad38612728a486a0d81
Rajko Albrecht <ral@alwins-world.de>
2019-02-16 09:53:12 +0100

    app.vue uses coffeescript

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -3,32 +3,29 @@
   <div id="app">
     <v-app id="dyndns">
         <confirm ref="confirm"></confirm>
-        <csrf></csrf>
         <app-navigation></app-navigation>
         <router-view></router-view>
     </v-app>
   </div>
 </template>
 
-<script>
-    import 'regenerator-runtime/runtime';
-    import CSRF from 'components/shared/csrf.vue'
+<script lang="coffee">
+    import 'regenerator-runtime/runtime'
     import Navigation from 'components/Navigation.vue'
     import confirm from 'components/shared/confirm'
 
     export default {
         components: {
-            csrf: CSRF,
-            appNavigation: Navigation,
-            confirm: confirm,
+            appNavigation: Navigation
+            confirm: confirm
         },
-        data: () => ({
-            message: "",
-        }),
-        mounted: function() {
-            console.log("app mounted")
+        data: () ->
+          {
+            message: "Töf"
+          }
+        ,
+        mounted: () ->
             this.$root.$confirm = this.$refs.confirm.open
-        }
     }
 </script>
 
