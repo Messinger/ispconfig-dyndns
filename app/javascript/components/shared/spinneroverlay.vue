5d6b2f22aa567dac855bd49e1d128971d82bf366
Rajko Albrecht <ral@alwins-world.de>
2019-02-18 20:48:03 +0100

    small spinner overlay test

---
+++ app/javascript/components/shared/spinneroverlay.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-dialog v-model="dialog" v-bind:style="{zIndex: 200}" persistent width="60">
+        <v-card color="white">
+            <v-card-text>
+                <v-progress-circular
+                        indeterminate
+                        color="black"
+                ></v-progress-circular>
+            </v-card-text>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script lang="coffee">
+    export default {
+        name: "spinneroverlay"
+        data: () ->
+            {
+              dialog: false
+            }
+        methods: {
+              open: () ->
+                  this.dialog = true
+              ,
+              close: () ->
+                  this.dialog = false
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
