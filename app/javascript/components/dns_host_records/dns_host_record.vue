3cfd969da8c3464cc2dbf3a522fc7653aa7cc4d5
Rajko Albrecht <ral@alwins-world.de>
2019-02-18 13:00:35 +0100

    validation

--- app/javascript/components/dns_host_records/dns_host_record.vue
+++ app/javascript/components/dns_host_records/dns_host_record.vue
@@ -3,46 +3,71 @@
         <v-breadcrumbs :items="br_items">
             <v-icon slot="divider">chevron_right</v-icon>
         </v-breadcrumbs>
-            <v-card>
-                <v-card-title primary-title>
-                    <h3 v-if="id !== 'new' && record.name !== null">
-                        {{record.name}}.{{record.dns_zone.name}}
-                    </h3>
-                </v-card-title>
-                <v-card-text>
-                    <v-form ref="recordform" v-model="valid" lazy-validation id="dnsrecordform">
-                        <v-layout row>
-                            <v-flex xs1>
-                                <v-subheader>IPv4 Adress:</v-subheader>
-                            </v-flex>
-                            <v-flex xs3>
-                                <v-text-field v-model="record.dns_host_ip_a_record.address"></v-text-field>
-                            </v-flex>
-                        </v-layout>
-                        <v-layout row>
-                            <v-flex xs1>
-                                <v-subheader>IPv6 Adress:</v-subheader>
-                            </v-flex>
-                            <v-flex xs3>
-                                <v-text-field v-model="record.dns_host_ip_aaaa_record.address"></v-text-field>
-                            </v-flex>
-                        </v-layout>
-                    </v-form>
-                </v-card-text>
-            </v-card>
+        <v-card v-if="!this.loading">
+            <v-card-title primary-title>
+                <h3 v-if="id !== 'new' && record.name !== null">
+                    {{record.name}}.{{record.dns_zone.name}}
+                </h3>
+            </v-card-title>
+            <v-card-text>
+                <v-form ref="form" v-model="valid" lazy-validation id="recordform">
+                    <v-layout v-if="this.id === 'new'" row>
+                        <v-flex xs1>
+                            <v-subheader>Name:</v-subheader>
+                        </v-flex>
+                        <v-flex xs1>
+                            <v-text-field v-model="record.name" suffix="." :rules="nameRules" label="Rechnername" required></v-text-field>
+                        </v-flex>
+                        <v-flex xs2>
+                            <v-select
+                                    v-model="record.dns_zone"
+                                    :items="allowed_dns_zones"
+                                    item-text="name"
+                                    item-value="id"
+                                    :rules="dnsRules"
+                                    label="DNS Zone"
+                                    required
+                            >
+                            </v-select>
+                        </v-flex>
+                    </v-layout>
+                    <v-layout row>
+                        <v-flex xs1>
+                            <v-subheader>IPv4 Adress:</v-subheader>
+                        </v-flex>
+                        <v-flex xs3>
+                            <v-text-field v-model="record.dns_host_ip_a_record.address"></v-text-field>
+                        </v-flex>
+                    </v-layout>
+                    <v-layout row>
+                        <v-flex xs1>
+                            <v-subheader>IPv6 Adress:</v-subheader>
+                        </v-flex>
+                        <v-flex xs3>
+                            <v-text-field v-model="record.dns_host_ip_aaaa_record.address"></v-text-field>
+                        </v-flex>
+                    </v-layout>
+                </v-form>
+            </v-card-text>
+            <v-card-actions>
+                <v-btn @click="validate_and_submit" flat color="success">Speichern</v-btn>
+            </v-card-actions>
+        </v-card>
     </div>
 </template>
 
 <script lang="coffee">
     export default {
         name: "dns_host_record"
-        props: ['id']
+        props: [
+            'id'
+        ]
         data: () ->
             {
                 loading: true
                 readonly: true
                 isdialog: true
-                valid: false
+                valid: true
                 default_values: {
                     id: null,
                     name: null,
@@ -57,13 +82,23 @@
                     }
                 }
                 record: {}
-                br_items: []
+                br_items: [
+                ]
+                allowed_dns_zones: [
+                ]
+                nameRules: [
+                    (v) -> (!!v || "Name wird benötigt")
+                    (v) -> ( (!!v && v.length <= 15) || 'Name sollte max. 15 Zeichen lang sein')
+                ]
+                dnsRules: [
+                    (v) ->
+                        !!Number.isInteger(v) || "Bitte Zone auswählen"
+                ]
             }
-        beforeMount: () ->
-            this.record = this.default_values
         mounted: () ->
-            console.log("Record view "+this.id+" mounted");
-            setTimeout(this.retrieve_record,50)
+            console.log("Record view " + this.id + " mounted");
+            setTimeout(this.retrieve_record, 50)
+            this.allowed_dns_zones = []
 
             this.br_items = [
                 {
@@ -72,18 +107,27 @@
                     href: this.$router.resolve('/').href
                 }
                 {
-                    text: "DNS Eintrage",
+                    text: "DNS Einträge",
                     disabled: false,
                     href: this.$router.resolve('/dns-host-records').href
                 }
             ]
         methods: {
             retrieve_record: () ->
-              result = await this.axios.get('/dns_host_records/'+this.id)
-              console.log(result)
-              if result.status == 200
-                  this.record = if this.id != 'new' then result.data else this.default_values
-                  this.readonly = (this.record.api_key == '' && this.record.id != null)
+                result = await this.axios.get('/dns_host_records/' + this.id)
+                console.log(result)
+                if result.status == 200
+                    this.record = if this.id != 'new' then result.data else this.default_values
+                    this.allowed_dns_zones = result.data.zoneselection if this.id == 'new'
+                    this.readonly = (this.record.api_key == '' && this.record.id != null)
+                this.loading = false
+
+            validate_and_submit: () ->
+                console.log "Validiere hostname"
+                return unless this.$refs.form.validate()
+
+
+                true
         }
     }
 </script>
