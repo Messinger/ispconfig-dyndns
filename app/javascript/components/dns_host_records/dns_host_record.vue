59767e30f2513ea849e5c8a9ccd4f00444a774f7
Rajko Albrecht <ral@alwins-world.de>
2019-02-18 09:12:54 +0100

    dns_host_record use coffeescript

--- app/javascript/components/dns_host_records/dns_host_record.vue
+++ app/javascript/components/dns_host_records/dns_host_record.vue
@@ -33,31 +33,32 @@
     </div>
 </template>
 
-<script>
+<script lang="coffee">
     export default {
-        name: "dns_host_record",
-        props: ['id'],
-        data: () => ({
-            loading: true,
-            readonly: true,
-            isdialog: true,
-            valid: false,
-            record: {
-                id: null,
-                name: null,
-                dns_zone: {
-                  name: null
-                },
-                dns_host_ip_a_record: {
-                    address: null
-                },
-                dns_host_ip_aaaa_record: {
-                    address: null
+        name: "dns_host_record"
+        props: ['id']
+        data: () ->
+            {
+                loading: true
+                readonly: true
+                isdialog: true
+                valid: false
+                record: {
+                    id: null,
+                    name: null,
+                    dns_zone: {
+                        name: null
+                    }
+                    dns_host_ip_a_record: {
+                        address: null
+                    }
+                    dns_host_ip_aaaa_record: {
+                        address: null
+                    }
                 }
-            },
-            br_items: []
-        }),
-        mounted: function () {
+                br_items: []
+            }
+        mounted: () ->
             console.log("Record view "+this.id+" mounted");
             setTimeout(this.retrieve_record,50)
             this.br_items = [
@@ -65,28 +66,24 @@
                     text: "Index",
                     disabled: false,
                     href: this.$router.resolve('/').href
-                },{
+                }
+                {
                     text: "DNS Eintrage",
                     disabled: false,
                     href: this.$router.resolve('/dns-host-records').href
                 }
             ]
-        },
         methods: {
-            async retrieve_record() {
-                if(this.id !== 'new') {
-                    console.log("Record");
-                    let result = await this.axios.get('/dns_host_records/'+this.id);
-                    console.log(result);
-                    if(result.status === 200) {
-                        this.record = result.data;
-                        this.readonly = this.record.api_key === '';
-                    }
-                } else {
-                    console.log("New record");
-                    this.readonly = false
-                }
-            }
+              retrieve_record: () ->
+                  if this.id != 'new'
+                      console.log("Record")
+                      result = await this.axios.get('/dns_host_records/'+this.id)
+                      console.log(result)
+                      if result.status == 200
+                          this.record = result.data
+                          this.readonly = this.record.api_key == ''
+                  else
+                    console.log "New record"
         }
     }
 </script>
