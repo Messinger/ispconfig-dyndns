d50e31853fbf69b78a151acce0191d4fa0e78acf
Rajko Albrecht <ral@alwins-world.de>
2019-02-21 11:43:39 +0100

    Blur out window if oauth-callback-window is active

--- app/javascript/components/shared/authproviders.vue
+++ app/javascript/components/shared/authproviders.vue
@@ -7,6 +7,9 @@
         </v-list-tile-action>
       </v-list-tile>
     </v-list>
+    <v-dialog v-model="lockdialog" persistent>
+      <v-container align-center justify-center  row fill-height><v-btn @click="login_window_focus">Hole das Authfenster zurück</v-btn></v-container>
+    </v-dialog>
   </div>
 </template>
 
@@ -15,10 +18,12 @@
     name: "authproviders"
     data: () ->
       {
+        lockdialog: false
         providerlist: []
         initialized: false
         promise: null
         reject: null
+        authwindow: null
       }
 
     mounted: () ->
@@ -31,10 +36,10 @@
         this.initialized = true
         retrieved = true
         providers = await this.axios.get('/users/providers').catch( (error) ->
-          console.log error
           retrieved = false
+          providers = {data: []}
         )
-        console.log providers
+        console.log "Auth providers: ",providers.data
         this.providerlist = if retrieved then providers.data else []
 
       oauth: (url) ->
@@ -43,21 +48,27 @@
       begin_login: (url) ->
         console.log window.$cookies.keys()
         window.$cookies.set("OAUTH-JSON-LOGIN",'1')
+        this.lockdialog = true
         user = await this.open_login_window(url,"Connect to my site",800,600)
+        this.lockdialog = false
         console.log "Got user via await: ",user
         window.$cookies.remove("OAUTH-JSON-LOGIN")
         if !!user && !!user.data
           window.Constants.current_user = user.data.account
           this.$root.$login_changed()
 
+      login_window_focus: () ->
+        this.authwindow.focus() if !!this.authwindow && !this.authwindow.closed
+
       open_login_window: (url,title,width,height) ->
         that = this
         left = screen.width / 2 - width/2
         top = screen.height / 2 - height / 2
-        authwindow = window.open url,title,"location=0,status=0,width=#{width},height=#{height},top=#{top},left=#{left}"
+        this.authwindow = window.open url,title,"location=0,status=0,width=#{width},height=#{height},top=#{top},left=#{left}"
         interval = setInterval( () ->
-          if authwindow.closed
+          if that.authwindow.closed
             clearInterval(interval)
+            that.authwindow = null
             user = await that.axios.get('/users/fetch_user').catch(
               (errors) ->
                 that.reject({})
