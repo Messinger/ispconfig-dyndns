a1e8df164cf387a966b7569838cd44849160b5db
Rajko Albrecht <ral@alwins-world.de>
2019-02-26 12:16:19 +0100

    Rules and beauty for settings form

--- app/javascript/components/admin/app_settings.vue
+++ app/javascript/components/admin/app_settings.vue
@@ -11,7 +11,7 @@
               <v-subheader>Mail from:</v-subheader>
             </v-flex>
             <v-flex md3 xs6>
-              <v-text-field v-model="settings.mail_from" required></v-text-field>
+              <v-text-field v-model="settings.mail_from" required :rules="emailRules"></v-text-field>
             </v-flex>
           </v-layout>
           <v-layout row wrap>
@@ -19,7 +19,7 @@
               <v-subheader>ISPConfig url:</v-subheader>
             </v-flex>
             <v-flex md3 xs6>
-              <v-text-field v-model="settings.ispconfig_url" required></v-text-field>
+              <v-text-field v-model="settings.ispconfig_url" :rules="textRules" required></v-text-field>
             </v-flex>
           </v-layout>
           <v-layout row wrap>
@@ -27,7 +27,7 @@
               <v-subheader>Remote User:</v-subheader>
             </v-flex>
             <v-flex md3 xs6>
-              <v-text-field v-model="settings.remote_user" required></v-text-field>
+              <v-text-field v-model="settings.remote_user" :rules="textRules" required></v-text-field>
             </v-flex>
           </v-layout>
           <v-layout row wrap>
@@ -39,6 +39,7 @@
                             :type="showpw ? 'text' : 'password'"
                             :append-icon="showpw ? 'visibility_off' : 'visibility'" required
                             @click:append="showpw=!showpw"
+                            :rules="textRules"
               >
               </v-text-field>
             </v-flex>
@@ -48,7 +49,7 @@
               <v-subheader>Max records per User:</v-subheader>
             </v-flex>
             <v-flex md3 xs6>
-              <v-text-field v-model="settings.max_records" required type="number" min="1"></v-text-field>
+              <v-text-field v-model="settings.max_records" required type="number" :rules="maxRecordsRules" min="1"></v-text-field>
             </v-flex>
           </v-layout>
           <v-layout row wrap>
@@ -56,13 +57,13 @@
               <v-subheader>Default TTL (sec):</v-subheader>
             </v-flex>
             <v-flex md3 xs6>
-              <v-text-field v-model="settings.default_ttl" required type="number" min="300"></v-text-field>
+              <v-text-field v-model="settings.default_ttl" required type="number" :rules="ttlRules" min="300" max="7200"></v-text-field>
             </v-flex>
           </v-layout>
         </v-form>
       </v-card-text>
       <v-card-actions>
-        <v-btn :disabled="!valid" color="primary darken-1" flat="flat" @click.native="submit_settings">Einstellungen speichern</v-btn>
+        <v-btn :disabled="!valid" :loading="saving" color="primary darken-1" flat="flat" @click.native="submit_settings">Einstellungen speichern</v-btn>
       </v-card-actions>
     </v-card>
   </div>
@@ -73,6 +74,7 @@
         name: "app_settings"
         data: () ->
           {
+            saving: false
             showpw: false
             valid: false
             settings: {
@@ -83,6 +85,35 @@
               max_records: ''
               default_ttl: ''
             }
+            textRules: [
+              (v) -> !!v || 'Required'
+            ]
+            ttlRules: [
+              (v) -> !!v || 'Required and must be an Number'
+              (v) -> !isNaN(v) || 'Must be number'
+              (v) ->
+                iv = parseInt(v)
+                if iv < 300
+                  'Must be >= 300'
+                else if iv > 7200
+                   'Must be <= 7200'
+                else
+                  true
+            ]
+            maxRecordsRules: [
+              (v) -> !!v || 'Required and must be an Number'
+              (v) -> !isNaN(v) || 'Must be number'
+              (v) ->
+                iv = parseInt(v)
+                if iv < 1
+                  'Must be >= 1'
+                else
+                  true
+            ]
+            emailRules: [
+              (v) -> !!v || 'E-mail is required'
+              (v) -> /.+@.+/.test(v) || 'E-mail must be valid'
+            ]
           }
 
         mounted: () ->
@@ -91,21 +122,27 @@
         methods: {
             fetch_settings: () ->
               that = this
+              this.saving = true
               this.axios.get('/settings')
                 .then(
                   (response) ->
-                    console.log response.data
+                    that.saving = false
                     that.settings = response.data.settings
               ).catch((error) ->
-                console.log error
+                that.saving = false
+                that.$root.$toast.error(error.data||'Fehler beim Laden der Einstellungen')
               )
 
             submit_settings: () ->
+              return unless this.$refs.form.validate()
               that = this
+              this.saving = true
               this.axios.post('/settings/edit',{settings: this.settings}).then( (response) ->
                 that.$root.$toast.success('Einstellungen gespeichert')
+                that.saving = false
               ).catch(
                   (error) ->
+                    that.saving = false
                     that.$root.$toast.error(error.data||'Fehler')
               )
         }
